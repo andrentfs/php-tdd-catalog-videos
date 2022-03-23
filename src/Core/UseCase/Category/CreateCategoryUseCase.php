15ac91f62c1e1da49c5160ac42c7ea3cdf55e63d
root <root@DESKTOP-JG81KME.localdomain>
2022-03-23 14:13:09 -0300

    alter

--- src/Core/UseCase/Category/CreateCategoryUseCase.php
+++ src/Core/UseCase/Category/CreateCategoryUseCase.php
@@ -4,6 +4,10 @@ namespace Core\UseCase\Category;
 
 use Core\Domain\Entity\Category;
 use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\DTO\Category\{
+    CategoryCreateInputDto,
+    CategoryCreateOutputDto
+};
 
 class CreateCategoryUseCase
 {
@@ -14,13 +18,21 @@ class CreateCategoryUseCase
         $this->repository = $repository;
     }
     
-    public function execute()
+    public function execute(CategoryCreateInputDto $input): CategoryCreateOutputDto
     {
         $category = new Category(
-            name: 'teste'
+            name: $input->name,
+            description: $input->description,
+            isActive: $input->isActive,
         );
 
-        $this->repository->insert($category);
-    }
+        $newCategory = $this->repository->insert($category);
 
+        return new CategoryCreateOutputDto(
+            id: $newCategory->id(),
+            name: $newCategory->name,
+            description: $category->description,
+            is_active: $category->isActive,
+        );
+    }
 }
---
+++ src/Core/UseCase/DTO/Category/CategoryCreateOutputDto.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Core\UseCase\DTO\Category;
+
+class CategoryCreateOutputDto
+{
+    public function __construct(
+        public string $id,
+        public string $name,
+        public string $description = '',
+        public bool $isActive = true,    
+    ) {}
+}
